343a5ffaa84236fbaf3b65461f768d6963958953
decandelac <33880366+decandelac@users.noreply.github.com>
2017-11-27 17:41:40 -0500

    Update Torre.java

--- Chess/src/Datos/Torre.java
+++ Chess/src/Datos/Torre.java
@@ -9,20 +9,29 @@ public class Torre extends Ficha {
  @Override
     public boolean MovimientoValido(int x, int y) {
         //verificar si esta ocupada la casilla.
-        if (x==getX()){
-            int a = -1; 
-            if (y>getY()){
-            // Va a sumar.
-            a = 1; 
-            } 
-            
-            for (int b = getY()+a; b < y; b=b+a ){
-            
-            
+        int mx,my; 
+        if ((x-getX()==0)||(y-getY()==0)){
+        
+       if(x<getX()){mx=-1;}
+            else if(x>getX()){mx=1;}
+            else{mx=0;}
+            if(y<getY()){my=-1;}
+            else if(y>getY()){my=1;}
+            else{my=0;}
+            boolean salir=false;
+            int vY=getY()+my ;
+           for(int vX=getX()+mx;(vX!=x&&mx!=0)||(vY!=y&&my!=0);vX=vX+mx){//recorrer cada posicion en x
+                if(Tablero.CasillaOcupada(vX,vY)!=0){//si está ocupada
+                    return false;//movimiento no válido
+                }
+            vY=vY+my;
             }
-            
+          return  vercasilla(x,y);
         }
-        return true; //CAAAAAAAAAAMBIIIIIIAAAAARRRRRRRRRRKJIEFNBÑOBIUEF
+        else{
+                return false;
+                }
+        
     }
     
     @Override
@@ -30,3 +39,4 @@ public class Torre extends Ficha {
         return "t";
     }
 }
+
