642edf3ed7cebd843048a80a1bb1e38408f87335
DiegoGAnaya <33880351+DiegoGAnaya@users.noreply.github.com>
2017-11-27 13:56:47 -0500

    Update Tablero.java

--- Chess/src/Datos/Tablero.java
+++ Chess/src/Datos/Tablero.java
@@ -8,16 +8,64 @@ public class Tablero {
         for(int i=0;i<8;i++){
             for(int j=0;j<8;j++){
                 switch(i){
+                    case 0:
+                        if(j==0){
+                            this.board[i][j]=new Torre(i,j'n');
+                        }
+                        else if(j==1){
+                            this.board[i][j]=new Caballo(i,j'n');
+                        }
+                        else if(j==2){
+                            this.board[i][j]=new Alfil(i,j'n');
+                        }
+                        else if(j==3){
+                            this.board[i][j]=new Reina(i,j'n');
+                        }
+                        else if(j==4){
+                            this.board[i][j]=new Rey(i,j'n');
+                        }
+                        else if(j==5){
+                            this.board[i][j]=new Alfil(i,j'n');
+                        }
+                        else if(j==6){
+                            this.board[i][j]=new Caballo(i,j'n');
+                        }
+                        else if(j==7){
+                            this.board[i][j]=new Torre(i,j'n');
+                        }
+                        else break;
                     case 1:
-                        break;
-                    case 2:
                         this.board[i][j]=new Peon(i,j,'n');
                         break;
-                    case 7:
+                    case 6:
                         this.board[i][j]=new Peon(i,j,'b');
                         break;
-                    case 8:
-                        break;
+                    case 7:
+                         if(j==0){
+                            this.board[i][j]=new Torre(i,j'b');
+                        }
+                        else if(j==1){
+                            this.board[i][j]=new Caballo(i,j'b');
+                        }
+                        else if(j==2){
+                            this.board[i][j]=new Alfil(i,j'b');
+                        }
+                        else if(j==3){
+                            this.board[i][j]=new Reina(i,j'b');
+                        }
+                        else if(j==4){
+                            this.board[i][j]=new Rey(i,j'b');
+                        }
+                        else if(j==5){
+                            this.board[i][j]=new Alfil(i,j'b');
+                        }
+                        else if(j==6){
+                            this.board[i][j]=new Caballo(i,j'b');
+                        }
+                        else if(j==7){
+                            this.board[i][j]=new Torre(i,j'b');
+                        }
+                        else break;
                     default:
                         this.board[i][j]=null;
                         break;
