08ff6f6b967d4906b9ce3d376f40d8310cf624d6
E5-473 <E5-473@10.203.128.161>
2017-11-27 16:06:01 -0500

    Impresion fichas

--- Chess/src/Datos/Tablero.java
+++ Chess/src/Datos/Tablero.java
@@ -5,69 +5,72 @@ public class Tablero {
     public static Ficha board[][]=new Ficha[8][8];
     public static Ficha muertas[][]=new Ficha[8][4];
     public Tablero() {
-        for(int j=0;j<8;j++){
-            for(int i=0;i<8;i++){
-                System.out.println(i+","+j);
-                switch(j){
+        for(int i=0;i<8;i++){
+            for(int j=0;j<8;j++){
+                
+                switch(i){
                     case 0:
-                        switch(i){
+                        switch(j){
                             case 0:
-                            this.board[i][j]=new Torre(i,j,'n');
+                            this.board[j][i]=new Torre(j,i,'n');
                         break;
                             case 1:
-                            this.board[i][j]=new Caballo(i,j,'n');
+                            this.board[j][i]=new Caballo(j,i,'n');
                         break;
                             case 2:
-                            this.board[i][j]=new Alfil(i,j,'n');
+                            this.board[j][i]=new Alfil(j,i,'n');
                         break;
                             case 3:
-                            this.board[i][j]=new Reina(i,j,'n');
+                            this.board[j][i]=new Reina(j,i,'n');
                         break;
                             case 4:
-                            this.board[i][j]=new Rey(i,j,'n');
+                            this.board[j][i]=new Rey(j,i,'n');
                         break;
                             case 5:
-                            this.board[i][j]=new Alfil(i,j,'n');
+                            this.board[j][i]=new Alfil(j,i,'n');
                         break;
                             case 6:
-                            this.board[i][j]=new Caballo(i,j,'n');
+                            this.board[j][i]=new Caballo(j,i,'n');
                         break;
                             case 7:
-                            this.board[i][j]=new Torre(i,j,'n');
+                            this.board[j][i]=new Torre(j,i,'n');
                             break;
                         }
                         break;
                     case 1:
-                        this.board[i][j]=new Peon(i,j,'n');
+                        this.board[j][i]=new Peon(j,i,'n');
                         break;
-                    case 6:
-                        this.board[i][j]=new Peon(i,j,'b');
+                    case 6:System.out.println(i+","+j);
+                        System.out.println(board[0][0].toString()+board[0][0].getColor());
+                        this.board[j][i]=new Peon(i,j,'b');
+                        System.out.println(board[0][0].toString()+board[0][0].getColor());
+                        System.out.println(board[i][j].toString()+board[i][j].getColor());
                         break;
                     case 7:
                         switch(i){
                             case 0:
-                            this.board[i][j]=new Torre(i,j,'b');
+                            this.board[j][i]=new Torre(i,j,'b');
                         break;
                             case 1:
-                            this.board[i][j]=new Caballo(i,j,'b');
+                            this.board[j][i]=new Caballo(i,j,'b');
                         break;
                             case 2:
-                            this.board[i][j]=new Alfil(i,j,'b');
+                            this.board[j][i]=new Alfil(i,j,'b');
                         break;
                             case 3:
-                            this.board[i][j]=new Reina(i,j,'b');
+                            this.board[j][i]=new Reina(i,j,'b');
                         break;
                             case 4:
-                            this.board[i][j]=new Rey(i,j,'b');
+                            this.board[j][i]=new Rey(i,j,'b');
                         break;
                             case 5:
-                            this.board[i][j]=new Alfil(i,j,'b');
+                            this.board[j][i]=new Alfil(i,j,'b');
                         break;
                             case 6:
-                            this.board[i][j]=new Caballo(i,j,'b');
+                            this.board[j][i]=new Caballo(i,j,'b');
                         break;
                             case 7:
-                            this.board[i][j]=new Torre(i,j,'b');
+                            this.board[j][i]=new Torre(i,j,'b');
                             break;
                         }
                         break;
@@ -75,6 +78,7 @@ public class Tablero {
                         this.board[i][j]=null;
                         break;
                 }
+                System.out.print(this.board[0][0].getColor());
             }
         }
     }
